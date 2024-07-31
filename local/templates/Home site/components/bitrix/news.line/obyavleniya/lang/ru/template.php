a5e098c979ba2ecd227e538ed28246577ae3a0f9
Your Name <you@example.com>
2024-07-31 11:02:49 +0300

    № 170073 Исправление

---
+++ local/templates/Home site/components/bitrix/news.line/obyavleniya/lang/ru/template.php
@@ -0,0 +1,9 @@
+<?
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
+$MESS["NEW_PROPERTIES"] = "Новые возможности для";
+$MESS["SQUARE"] = "Площадь:";
+$MESS["BATH"] = "Санузел:";
+$MESS["BEDROOM"] = "Этажей:";
+$MESS["GARAGES"] = "Гаражи:";
+
+?>
